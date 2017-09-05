d08f581b93acbf253136e226d8eb07605e50973f
Helene Wagner <hhwagner1@gmail.com>
2017-09-04 20:39:18 -0400

    Added github dependencies

--- R/startTutorialAddin.R
+++ R/startTutorialAddin.R
@@ -1,18 +1,20 @@
 startTutorialAddin <- function() {
-  require(swirl)
-  install_course_directory("Landscape_Genetic_Data_Analysis_with_R.swc")
+#  swirl::install_course_directory("Landscape_Genetic_Data_Analysis_with_R.swc")
 #  install_course_directory('system.file("extdata",
 #                           "Landscape_Genetic_Data_Analysis_with_R.swc",
 #                           package = "TestCoursePackage")')
-
+  path <- system.file('swirl/Landscape_Genetic_Data_Analysis_with_R.swc', package='TestCoursePackage')
+  swirl::install_course_directory(path)
 
   cat("To start a tutorial:","\n",
+      "- type: require(swirl)", "\n",
       "- type: swirl()", "\n",
       "- follow prompts", "\n",
-      "- select course and tutorial","\n",
-      "- to exit, type: bye()","\n",
-      "- to restart tutorial, use a different name", "\n",
-      "  (best add a number, like this: 'MyName2')")
+      "- select course and tutorial","\n\n")
+#  cat("- to exit, type: bye()","\n",
+#      "- to resume, type: swirl()","\n",
+#      "- to restart tutorial, use a different name", "\n",
+#      "  (best add a number, like this: 'MyName2')")
 }
 
 
