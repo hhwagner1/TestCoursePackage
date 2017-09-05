435476c75ed8ea3a7d7be0ae89cc5661a949ab87
Helene Wagner <hhwagner1@gmail.com>
2017-09-05 19:09:56 -0400

    Added watchVideoAddin

---
+++ R/watchVideoAddin.R
@@ -0,0 +1,34 @@
+watchVideoAddin <- function() {
+
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar("Watch a Course Video"),
+    miniUI::miniContentPanel(
+
+      shiny::selectInput("video", "Select a Video:",
+                   list("Week 1: Getting LG Data into R"=1),
+                  selected = 1
+      )
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    # Listen for 'done' events.
+    shiny::observeEvent(input$done, {
+
+      selectedVideo <- c("http://sho.co/199ZO")[as.numeric(input$video)]
+      utils::browseURL(selectedVideo)
+
+      cat(paste0("Opening ",selectedVideo, " in web browser.", "\n\n",
+      "Hint: make video fullscreen by clicking on screen symbol on bottom right."))
+
+      shiny::stopApp()
+    })
+
+  }
+
+  viewer <- shiny::dialogViewer("Watch a Course Video",
+                                width = 400, height = 600)
+  shiny::runGadget(ui, server, viewer = viewer)
+
+}
