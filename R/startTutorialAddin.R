1a3b18af1fcdf2803655ec628e7f9baf9f3754cf
Helene Wagner <hhwagner1@gmail.com>
2017-09-05 20:16:52 -0400

    Prepared github link to swirl course

--- R/startTutorialAddin.R
+++ R/startTutorialAddin.R
@@ -12,6 +12,8 @@ startTutorialAddin <- function() {
 
   swirl::install_course_dropbox("https://www.dropbox.com/s/2vxdiyg7fn9r2mb/Landscape_Genetic_Data_Analysis_with_R.swc.zip?dl=0")
 
+  #install_course_github("hhwagner1", "Landscape_Genetics_R_Course")
+
   cat("\n","To start a tutorial:","\n",
       "- type: require(swirl)", "\n",
       "- type: swirl()", "\n",
