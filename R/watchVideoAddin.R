e2e73395069e6a99f52fc07187bb6db9cf577a9a
Helene Wagner <hhwagner1@gmail.com>
2017-09-15 19:43:59 -0400

    Addes slides option to videos

--- R/watchVideoAddin.R
+++ R/watchVideoAddin.R
@@ -4,7 +4,13 @@ watchVideoAddin <- function() {
     miniUI::gadgetTitleBar("Watch a Course Video"),
     miniUI::miniContentPanel(
 
-      shiny::selectInput("video", "Select a Video:",
+      shiny::selectInput("type", "What do you want to open?",
+                         list("Video only (in web browser)"=1,
+                              "Slides only (as PDF)"=2,
+                              "Video and slides"=3),
+                         selected = 1
+      ),
+      shiny::selectInput("video", "Select a Topic:",
                    list("Week 1: Getting LG Data into R"=1,
                         "Week 2: Land Cover Analysis in R"=2),
                   selected = 1
@@ -22,15 +28,16 @@ watchVideoAddin <- function() {
 
       selectedSlides <- c("Week1_Slides.pdf",
                           "Week2_Slides.pdf")[as.numeric(input$video)]
-
-      utils::browseURL(selectedVideo)
-      utils::browseURL(paste0('file://',
-                              system.file("Slides", selectedSlides,
-                                          package = "TestCoursePackage")))
-
+      if(as.numeric(input$type) != 2) utils::browseURL(selectedVideo)
+      if(as.numeric(input$type) != 1) utils::browseURL(paste0('file://',
+                    system.file("Slides", selectedSlides,
+                                 package = "TestCoursePackage")))
 
       cat(paste0("Opening ",selectedVideo, " and slides in web browser.", "\n\n",
-      "Hint: make video fullscreen by clicking on screen symbol on bottom right."))
+      "Hints:
+      - Videos will open in your default web browser.
+      - Slides will open in your default app for PDF files.
+      - Make the video fullscreen by clicking on screen symbol on bottom right."))
 
       shiny::stopApp()
     })
