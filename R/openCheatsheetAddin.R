d0d450522158aecaa6506926af6559a93100a64d
Helene Wagner <hhwagner1@gmail.com>
2017-09-16 17:31:51 -0400

    Changed method in download.file to 'curl'

--- R/openCheatsheetAddin.R
+++ R/openCheatsheetAddin.R
@@ -37,7 +37,7 @@ openCheatsheetAddin <- function() {
 
     utils::download.file(selectedFile,
                          destfile=file.path("downloads", basename(selectedFile)),
-                         method="auto")
+                         method="curl")
     utils::browseURL(file.path("downloads", basename(selectedFile)))
 
     cat(paste0("Hints:
